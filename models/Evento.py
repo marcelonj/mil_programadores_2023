314020330d462203cee9c7340bd35b56ca504562
fernando <fernandobenitez79@yahoo.com.ar>
2023-07-19 15:53:53 -0300

    se creo clase Evento y carpetas de estructura del proyecto

---
+++ models/Evento.py
@@ -0,0 +1,13 @@
+class Evento:
+    def __init__(self, id, nombre, artista, genero, id_ubicacion, hora_inicio, hora_fin, descripcion, imagen):
+        self.id = id
+        self.nombre = nombre
+        self.artista = artista
+        self.genero = genero
+        self.id_ubicacion = id_ubicacion
+        self.hora_inicio = hora_inicio
+        self.hora_fin = hora_fin
+        self.descripcion = descripcion
+        self.imagen = imagen
+
+    
