afe89649845e390f5f1022e32a0242bc4d2e2e60
diegolu7 <diegolu7@gmail.com>
2023-07-19 11:18:53 -0300

    feat: define class Review

---
+++ Clase/Review.py
@@ -0,0 +1,8 @@
+class Review:
+    def __init__(self, id, id_evento, id_usuario, calificacion, comentario, animo):
+        self.id = id
+        self.id_evento = id_evento
+        self.id_usuario = id_usuario
+        self.calificacion = calificacion
+        self.comentario = comentario
+        self.animo = animo
