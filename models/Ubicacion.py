d68b8a261e010aca4876fbd901c316b976722a2f
Marcelo Jurado <marcelonjurado@gmail.com>
2023-07-19 21:04:46 -0300

    feat: Cracion de metodos json de clases Ubicacion y RutaVisita

--- models/Ubicacion.py
+++ models/Ubicacion.py
@@ -1,6 +1,21 @@
+import json
+
 class Ubicacion:
     def __init__(self, id, nombre, direccion, coordenadas):
         self.id = id
         self.nombre = nombre
-        self.deireccion = direccion
+        self.direccion = direccion
         self.coordenadas = coordenadas
+
+    def to_json(self):
+        return {
+            "id" : self.id,
+            "nombre" : self.nombre,
+            "destinos" : self.direccion,
+            "coordenadas" : self.coordenadas 
+        }
+
+    @classmethod
+    def from_json(cls, json_str):
+        data = json.load(json_str)
+        return cls(data["id"], data["nombre"], data["direccion"], data["coordenadas"])
