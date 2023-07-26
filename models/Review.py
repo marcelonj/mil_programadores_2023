ed25371856da6cd066e156e675ec2cec6b12f69d
diegolu7 <diegolu7@gmail.com>
2023-07-26 20:14:54 -0300

    feat: complete module review

--- models/Review.py
+++ models/Review.py
@@ -23,3 +23,27 @@ class Review:
     def from_json(cls, json_str):
         data = json.loads(json_str)
         return cls(data['id'], data['id_evento'], data['id_usuario'], data['calificacion'], data['comentario'], data['animo'])
+
+    @classmethod
+    def load_reviews(cls, file_path):
+        reviews = []
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            for review_data in data:
+                review = cls(**review_data)
+                reviews.append(review)
+        return reviews
+
+    @classmethod
+    def save_reviews(cls, file_path, reviews):
+        data = [review.to_json() for review in reviews]
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    @staticmethod
+    def find_by_event_id(reviews, event_id):
+        return [review for review in reviews if review.id_evento == event_id]
+
+    @staticmethod
+    def find_by_user_id(reviews, user_id):
+        return [review for review in reviews if review.id_usuario == user_id]
